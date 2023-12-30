8601e78d7ba443ff14ab2a2d01344cc135edde67
Jhone <jleandro.dev@gmail.com>
2023-12-30 11:04:14 -0300

    feat: Model jobs implementado

--- jobs/models.py
+++ jobs/models.py
@@ -1,9 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Job(models.Model):
+    LEVEL_CHOICES = (
+        ('jr', 'Júnior'),
+        ('pl', 'Pleno'),
+        ('sr', 'Sênior'),
+    )
+    title = models.CharField(max_length=100, null=False, blank=False)
+    description = models.TextField(null=False, blank=False)
+    requirements = models.TextField()
+    responsibilities = models.TextField()
+    level = models.CharField(max_length=2, choices=LEVEL_CHOICES)
+    skills = models.ManyToManyField('jobs.Skill', related_name='jobs')
+
+
+    def __str__(self):
+        return self.title
 
 class Skill(models.Model):
     title = models.CharField(max_length=100, unique=True, blank=False, null=False)
 
     def __str__(self):
         return self.title
+    
